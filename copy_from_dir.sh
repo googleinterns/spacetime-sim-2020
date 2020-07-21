91bc9f09a00d41d43388267dc6de931dd2180bbe
gilbert-bahati <gilbertbahati@google.com>
2020-07-21 00:05:11 +0000

    added commands to copy new files

--- copy_from_dir.sh
+++ copy_from_dir.sh
@@ -6,6 +6,10 @@ cp Gilbert_code/single_agent/__init__.py ~/flow/flow/envs
 cp Gilbert_code/multi_agent/traffic_light_grid.py ~/flow/flow/envs/multiagent
 cp Gilbert_code/multi_agent/__init__.py ~/flow/flow/envs/multiagent
 
+#changes added
+cp Gilbert_code/single_agent/presslight_single_agent.py ~/flow/flow/envs
+cp Gilbert_code/multi_agent/presslight_multi_agent.py ~/flow/flow/envs/multiagent
+
 #experiments
 #non-rl
 cp Gilbert_code/single_agent/grid1x1.py ~/flow/examples/exp_configs/non_rl
@@ -21,3 +25,6 @@ cp Gilbert_code/multi_agent/grid2x2_rl.py ~/flow/examples/exp_configs/rl/multiag
 cp Gilbert_code/utils_and_source_code_edits/util.py ~/flow/flow/core
 cp Gilbert_code/utils_and_source_code_edits/traci.py ~/flow/flow/core/kernel/simulation
 cp Gilbert_code/utils_and_source_code_edits/train.py ~/flow/examples
+
+#changes added
+cp Gilbert_code/utils_and_source_code_edits/presslight_utils.py ~/flow/flow/core
--- copy_to_dir.sh
+++ copy_to_dir.sh
@@ -6,6 +6,11 @@ cp ~/flow//flow/envs/__init__.py Gilbert_code/single_agent
 cp ~/flow/flow/envs/multiagent/traffic_light_grid.py Gilbert_code/multi_agent
 cp ~/flow/flow/envs/multiagent/__init__.py Gilbert_code/multi_agent
 
+#changes added
+cp ~/flow/flow/envs/presslight_single_agent.py Gilbert_code/single_agent
+cp ~/flow/flow/envs/multiagent/presslight_multi_agent.py Gilbert_code/multi_agent
+
+
 #experiments
 #non-rl
 cp ~/flow/examples/exp_configs/non_rl/grid1x1.py Gilbert_code/single_agent
@@ -21,3 +26,6 @@ cp ~/flow/examples/exp_configs/rl/multiagent/grid2x2_rl.py Gilbert_code/multi_ag
 cp ~/flow/flow/core/util.py Gilbert_code/utils_and_source_code_edits
 cp ~/flow/flow/core/kernel/simulation/traci.py Gilbert_code/utils_and_source_code_edits
 cp ~/flow/examples/train.py Gilbert_code/utils_and_source_code_edits
+
+#changes added
+cp ~/flow/flow/core/presslight_utils.py Gilbert_code/utils_and_source_code_edits
