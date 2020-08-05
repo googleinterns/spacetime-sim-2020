0fccbc3b9f74bbee2af3482fbbdc421847749aa9
gilbert-bahati <gilbertbahati@google.com>
2020-08-05 21:56:20 +0000

    restructured directory names

--- copy_to_dir.sh
+++ copy_to_dir.sh
@@ -9,19 +9,20 @@ cp ~/flow/flow/envs/multiagent/__init__.py Gilbert_code/multi_agent
 #changes added
 #cp ~/flow/flow/envs/presslight_single_agent.py Gilbert_code/single_agent
 #cp ~/flow/flow/envs/multiagent/presslight_multi_agent.py Gilbert_code/multi_agent
-cp ~/flow/flow/envs/centralized_multi_agent_thesis.py Gilbert_code/single_agent
-cp ~/flow/flow/envs/centralized_single_agent_presslight.py Gilbert_code/single_agent
-cp ~/flow/flow/envs/multiagent/decentralized_multi_light_presslight.py Gilbert_code/multi_agent
-cp ~/flow/flow/envs/multiagent/decentralized_multi_light_thesis.py Gilbert_code/multi_agent
+cp ~/flow/flow/envs/centralized_env.py Gilbert_code/centralized
+cp ~/flow/flow/envs/multiagent/decentralized_multi_light_presslight.py Gilbert_code/decentralized
+cp ~/flow/flow/envs/multiagent/decentralized_multi_light_thesis.py Gilbert_code/decentralized
 
 #experiments
 #non-rl
-cp ~/flow/examples/exp_configs/non_rl/grid_simulation_non_rl.py Gilbert_code/multi_agent
+cp ~/flow/examples/exp_configs/non_rl/grid_simulation_non_rl.py Gilbert_code/benchmarks
+cp ~/flow/flow/envs/thesis.py Gilbert_code/benchmarks
+cp ~/flow/flow/envs/presslight.py Gilbert_code/benchmarks
 
 #rl
-cp ~/flow/examples/exp_configs/rl/singleagent/grid1x1_rl.py Gilbert_code/single_agent
-cp ~/flow/examples/exp_configs/rl/multiagent/grid1x3_rl.py Gilbert_code/multi_agent
-cp ~/flow/examples/exp_configs/rl/multiagent/grid2x2_rl.py Gilbert_code/multi_agent
+#cp ~/flow/examples/exp_configs/rl/singleagent/grid1x1_rl.py Gilbert_code/single_agent
+#cp ~/flow/examples/exp_configs/rl/multiagent/grid1x3_rl.py Gilbert_code/multi_agent
+#cp ~/flow/examples/exp_configs/rl/multiagent/grid2x2_rl.py Gilbert_code/multi_agent
 
 #utils and source_code_edits
 cp ~/flow/flow/core/util.py Gilbert_code/utils_and_source_code_edits/utils/
