f1df5f0e0937e278b1b822a1859a4c212817c8ea
gilbert-bahati <gilbertbahati@google.com>
2020-08-11 21:33:25 +0000

    broke up the get_state function

--- Gilbert_code/benchmarks/presslight.py
+++ Gilbert_code/benchmarks/presslight.py
@@ -41,84 +41,48 @@ class PressureDecentLightGridEnv:
 
         # Traffic light information
         direction = direction.flatten()
-        currently_yellow = currently_yellow.flatten()
         # This is a catch-all for when the relative_node method returns a -1
         # (when there is no node in the direction sought). We add a last
         # item to the lists here, which will serve as a default value.
         # TODO(cathywu) are these values reasonable?
         direction = np.append(direction, [0])
-        currently_yellow = np.append(currently_yellow, [1])
 
         obs = {}
-        # TODO(cathywu) allow differentiation between rl and non-rl lights
 
         # collect list of names of inner edges
-        internal_edges = []
-        for i in kernel.network.rts:
-            if kernel.network.rts[i]:
-                if kernel.network.rts[i][0][0][1:-1]:
-                    internal_edges += [kernel.network.rts[i][0][0][1:]]
-
+        internal_edges = get_internal_edges(kernel)
         node_to_edges = network.node_mapping
 
-        all_ids_incoming = dict()
         for rl_id in kernel.traffic_light.get_ids():
-            all_ids_incoming[rl_id] = []
-
             # collect observations for each traffic light
-            rl_id_num = int(rl_id.split("center")[ID_IDX])
-            local_edges = node_to_edges[rl_id_num][1]
-            local_edge_numbers = [kernel.network.get_edge_list().index(e)
-                                  for e in local_edges]
-            local_id_nums = [rl_id_num, _get_relative_node(rl_id, "top"),
-                             _get_relative_node(rl_id, "bottom"),
-                             _get_relative_node(rl_id, "left"),
-                             _get_relative_node(rl_id, "right")]
+
+            # collect and set edge names and ids
+            incoming_edges, local_edge_numbers, local_id_nums = get_traffic_params(rl_id,
+                                                                                node_to_edges,
+                                                                                kernel,
+                                                                                _get_relative_node)
 
             # define incoming edges
-            incoming = local_edges
-            outgoing_edges = []
-            edge_pressure = []
-            for edge_ in incoming:
-                if kernel.network.rts[edge_]:
-                    # if edge is an outer(global) incoming edge,
-                    # outgoing edge is the next edge in the route
-                    index_ = kernel.network.rts[edge_][0][0].index(edge_)
-                    outgoing_edges = kernel.network.rts[edge_][0][0][index_ + 1]
-                else:
-                    for lst in internal_edges:
-                        # if edge is an inner edges, outgoing is the next edge in the list
-                        if len(lst) > 1 and edge_ in lst:
-                            index_ = lst.index(edge_)
-                            outgoing_edges = lst[index_ + 1]
+            edge_pressure_state = []
+            for edge_ in incoming_edges:
 
-                # get vehicle ids in incoming edge
-                observed_ids = \
-                    get_id_within_dist(edge_, "ahead", kernel, self.benchmark_params)
-                all_ids_incoming[rl_id] += observed_ids
+                # get outgoing edge
+                outgoing_edge = get_outgoing_edge(kernel, edge_, internal_edges)
 
-                # get ids in outgoing edge
-                observed_ids_behind = \
-                    get_id_within_dist(outgoing_edges, "behind", kernel, self.benchmark_params)
+                # collect observed ids within distances and color them
+                observed_ids_ahead, observed_ids_behind = get_observed_ids(kernel,
+                                                                           edge_,
+                                                                           outgoing_edge,
+                                                                           self.benchmark_params)
 
-                # get edge pressures
-                edge_pressure += [len(observed_ids) - len(observed_ids_behind)]
-
-                # color incoming and outgoing vehicles
-                color_vehicles(observed_ids, self.benchmark_params.CYAN, kernel)
-                color_vehicles(observed_ids_behind, self.benchmark_params.RED, kernel)
+                # get edge pressures of those edges
+                edge_pressure_state += [len(observed_ids_ahead) - len(observed_ids_behind)]
 
             # for each incoming edge, store the pressure terms to be used in compute reward
-            self.edge_pressure_dict[rl_id] = edge_pressure
+            self.edge_pressure_dict[rl_id] = edge_pressure_state
 
-            light_states = kernel.traffic_light.get_state(rl_id)
-
-            if light_states == "GrGr":
-                light_states_ = [1]
-            elif light_states == ["yryr"]:
-                light_states_ = [0.6]
-            else:
-                light_states_ = [0.2]
+            # for each  intersection, collect traffic light state
+            light_states_ = get_light_states(kernel, rl_id)
 
             observation = np.array(np.concatenate(
                 [self.edge_pressure_dict[rl_id],
@@ -164,3 +128,98 @@ class PressureCentLightGridEnv(PressureDecentLightGridEnv):
 
         final_obs = np.concatenate(list((obs.values())))
         return final_obs
+
+
+def get_internal_edges(kernel):
+    """Collect all internal edges in network
+    ie. Inner edges are Internal edges
+                  (outer)         (outer)       (outer)
+                     |              |              |
+        (outer) x----|----Inner-----|----Inner-----|----x (outer)
+                   Inner          Inner          Inner
+        (outer) x----|----Inner-----|----Inner-----|----x (outer)
+                     |              |              |
+                 (outer)         (outer)       (outer)
+
+
+    """
+    internal_edges = []
+    for i in kernel.network.rts:
+        if kernel.network.rts[i]:
+            if kernel.network.rts[i][0][0][1:-1]:
+                internal_edges += [kernel.network.rts[i][0][0][1:]]
+    return internal_edges
+
+
+def get_outgoing_edge(kernel, edge_, internal_edges):
+    """Collect the next edge for vehicles given the incoming edge id""
+    ie.
+                    incoming
+                        |
+        -> --incoming---|---outgoing--->
+                        |
+                     outgoing
+    """
+    if kernel.network.rts[edge_]:
+        # if edge is an outer(global) incoming edge,
+        # outgoing edge is the next edge in the route
+        index_ = kernel.network.rts[edge_][0][0].index(edge_)
+        outgoing_edge = kernel.network.rts[edge_][0][0][index_ + 1]
+    else:
+        for lst in internal_edges:
+            # if edge is an inner edges, outgoing is the next edge in the list
+            if len(lst) > 1 and edge_ in lst:
+                index_ = lst.index(edge_)
+                outgoing_edge = lst[index_ + 1]
+
+    return outgoing_edge
+
+
+def get_light_states(kernel, rl_id):
+    """Return current traffic light stats as number:
+    ie either GrGrGr, yryryr, ryryry, rGrGrG"""
+
+    light_states = kernel.traffic_light.get_state(rl_id)
+
+    if light_states == "GrGr":
+        light_states__ = [1]
+    elif light_states == ["yryr"]:
+        light_states__ = [0.6]
+    else:
+        light_states__ = [0.2]
+
+    return light_states__
+
+
+def get_observed_ids(kernel, edge_, outgoing_edge, benchmark_params):
+    """Return lists of observed vehicles within look ahead and behind distances"""
+
+    # get vehicle ids in incoming edge
+    observed_ids_ahead = \
+        get_id_within_dist(edge_, "ahead", kernel, benchmark_params)
+
+    # get ids in outgoing edge
+    observed_ids_behind = \
+        get_id_within_dist(outgoing_edge, "behind", kernel, benchmark_params)
+
+    # color incoming and outgoing vehicles
+    color_vehicles(observed_ids_ahead, benchmark_params.CYAN, kernel)
+    color_vehicles(observed_ids_behind, benchmark_params.RED, kernel)
+
+    return observed_ids_ahead, observed_ids_behind
+
+
+def get_traffic_params(rl_id, node_to_edges, kernel, _get_relative_node):
+
+    """Collect ids and names of edges"""
+
+    rl_id_num = int(rl_id.split("center")[ID_IDX])
+    local_edges = node_to_edges[rl_id_num][1]
+    local_edge_numbers = [kernel.network.get_edge_list().index(e)
+                          for e in local_edges]
+    local_id_nums = [rl_id_num, _get_relative_node(rl_id, "top"),
+                     _get_relative_node(rl_id, "bottom"),
+                     _get_relative_node(rl_id, "left"),
+                     _get_relative_node(rl_id, "right")]
+
+    return local_edges, local_edge_numbers, local_id_nums
