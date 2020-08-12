7cdcda14093c67c85f05a710007a55f52850689f
gilbert-bahati <gilbertbahati@google.com>
2020-08-12 22:51:44 +0000

    documented and added comments in params class

--- Gilbert_code/benchmarks/benchmark_params.py
+++ Gilbert_code/benchmarks/benchmark_params.py
@@ -1,51 +1,65 @@
 from tensorboardX import SummaryWriter
 import os
-from datetime import datetime
 
 
 class BenchmarkParams:
+    """
+    Parameters required by environments:
+       mostly contains tensorboard setup and naming parameters,
+       flags for when to log info on tensorboard,
+       saving simulation results,
+       training flags such as actuated baselines
+
+    """
 
     def __init__(self):
-        now = datetime.now()
-        current_time = now.strftime("%Y-%H-%M-%S")
+
+        # specify path for tensorboard logging avg travel times, avg reward, no. of vehicles in network
         home_dir = os.path.expanduser('~')
         if not os.path.exists(home_dir + '/ray_results/real_time_metrics'):
             os.makedirs(home_dir + '/ray_results/real_time_metrics')
-
         self.summaries_dir = home_dir + '/ray_results/real_time_metrics'
-        self.log_rewards_during_iteration = False
-        self.sumo_actuated_baseline = False
-        self.save_plots = False
-        self. exp_being_run = "masters demand sumo"
-        self.experiment = "Presslight"
-        self.during_simulation = False
 
-        # choose look-ahead distance
-        # self.look_ahead = 80
-        # self.look_ahead = 160
-        # self.look_ahead = 240
+        # logging evolution of reward during a simulation
+        self.log_rewards_during_iteration = False
+
+        # activate sumo actuated baslines during training for first 6 iterations
+        self.sumo_actuated_baseline = True
+        self.sumo_actuated_simulations = 6
+
+        # plot and save histogram of travel time distribution at end of simulation
+        self.save_plots = False
+        # name and title of histogram (from above) to be saved
+        self. exp_being_run = "TEST_SUMO"
+
+        # choose look-ahead distance can be either 43, 80, 160 or 240
         self.look_ahead = 43
 
-        # choose demand pattern
+        # choose demand pattern for naming the tensorboard directory logging
         # self.demand = "L"
-        self.demand = "H_yebo"
+        self.demand = "THESIS_CENTRALIZED_B"
 
-        # choose exp running
+        # choose exp running, can either be "rl" or "non_rl"
         self.exp = "rl"
-        # exp = "non_rl"
 
         # log title for tensorboard
         self.log_title = '/simulation_1x3_analysis_{}_{}_{}'.format(self.exp, self.look_ahead, self.demand)
         self.filename = "/iterations_{}_{}.csv".format(self.look_ahead, self.demand)
         root_dir = os.path.expanduser('~')
+
+        # file location to store simulation numbers csv while training
+        # ie. if the training is at simulation 50, the last value add the created csv file, will be 50
+        # this helps us keep track of the average travel times, rewards and other values at the end
+        # of each simulation
         file_location = root_dir + '/ray_results/grid1x3_learning_rate_0.01'
         self.full_path = file_location + self.filename
 
+        # RGB colors for vehicles
         self.RED = (255, 0, 0)
         self.BLUE = (0, 0, 255)
         self.CYAN = (0, 255, 255)
 
-        # set up tensorboard logging info
+        # set up more tensorboard logging info
         self.exp_name = self.log_title
         self.writer = SummaryWriter(self.summaries_dir + self.exp_name)
         self.rew_list = []
